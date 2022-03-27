73bf97787844f6cb722c6301aa7d59056bb8dfee
Geahn <74743938+geahn@users.noreply.github.com>
2022-03-27 12:48:54 -0300

    Sandbox

--- src/App.js
+++ src/App.js
@@ -1,48 +1,45 @@
-import React, {useState} from "react";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 import Login from "./Comps/Login";
 import Cadastro from "./Comps/Cadastro";
 import Listagem from "./Comps/Listagem";
-import Splash from "./Comps/Splash"
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-        usuario: "",
-        senha:   ""
-    }
+      usuario: "",
+      senha: ""
+    };
   }
 
-  enviarDadosLogin = () => {
-    
-  }
+  enviarDadosLogin = () => {};
 
   render() {
     return (
       <div className="App">
         <Router>
           <Routes>
-            <Route path='/'
-                  element={<Login
-                    usuario={this.state.usuario}
-                    senha={this.state.senha} />
-                    }/>
-            <Route path='/login'
-                  element={<Login
-                    usuario={this.state.usuario}
-                    senha={this.state.senha} />
-                    }/>
-            <Route path="/cadastro"
-                  element={<Cadastro />}/>
-            <Route path="/listagem"
-                  element={<Listagem />}/>
+            <Route
+              path="/"
+              element={
+                <Login usuario={this.state.usuario} senha={this.state.senha} />
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <Login usuario={this.state.usuario} senha={this.state.senha} />
+              }
+            />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/listagem" element={<Listagem />} />
           </Routes>
         </Router>
       </div>
-    )
-}
+    );
+  }
 }
 
 export default App;
